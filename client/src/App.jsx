14b305942bde7ed9349c4941680f1f0a11976845
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-14 19:33:52 +0530

    adding header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,6 +20,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
