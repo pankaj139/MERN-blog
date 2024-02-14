f2abceeac00ffb7794fc25dea6f3161a42f1da90
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-14 18:11:10 +0530

    adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-export default function App(){
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+
+export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
-} 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
