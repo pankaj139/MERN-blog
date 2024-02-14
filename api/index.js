c0d980857b2f0e837f6352571ee00a8dac73274d
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-14 20:05:08 +0530

    added server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Mongodb is connected");
+}).catch((err) => {
+    console.log(err);
+})
 const app = express();
 const port = 3000;
 
