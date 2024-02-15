9d0ccca4226c4e2550136c6855b15d82b2a72a26
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-15 21:25:32 +0530

    Update user route to use updateUser controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,29 +1,7 @@
 import express from 'express';
+import {updateUser} from '../controllers/user.controller';
 const router = express.Router();
 
-// POST /users
-router.post('/', (req, res) => {
-    // Get the user data from the request body
-    const { name, email } = req.body;
-
-    // Create a new user object
-    const user = {
-        name,
-        email
-    };
-
-    // Send the user object as JSON response
-    res.json(user);
-});
-// GET /users
-router.get('/', (req, res) => {
-    res.json({message:"test"});
-});
-
-// PUT /users/:id
-router.put('/:id', (req, res) => {
-    // Logic for updating a user
-});
-
+router.put('/update', updateUser);
 
 export default router;
