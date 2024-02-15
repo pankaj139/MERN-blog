dacab7d1a2e7382ad09f3c75f1eccbb6e8668906
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-15 17:07:55 +0530

    Fix header link and add conditional rendering for profile tab in Dashboard

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,16 +12,16 @@ export default function Header() {
   const dispatch = useDispatch();
   return (
     <Navbar className='border-b-2'>
-      <Link
+    <Link
         to={'/'}
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
-      >
+    >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          {' '}
-          Pankaj's
+            {' '}
+            Pankaj&apos;s
         </span>
         Blog
-      </Link>
+    </Link>
       <form>
         <TextInput
           type='text'
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSideBar from '../components/DashSideBar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab,settab] = useState();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl) settab(tabFromUrl);
+
+  },[location.search]);
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* SideBar */}
+        <DashSideBar />
+      </div>
+      {/* Main Content */}
+      { tab=='profile' && <DashProfile />}
+    </div>
   )
 }
