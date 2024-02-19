bb74f17a7a86171eef6ea6fc44c4bbc505c55e97
Pankaj Khandelwal <pankaj139@gmail.com>
2024-02-19 12:46:30 +0530

    Add getUsers endpoint to user controller and route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {update,deleteUser,signout} from '../controllers/user.controller.js';
+import {update,deleteUser,signout,getUsers} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.put('/update/:userId',verifyToken, update);
 router.delete('/delete/:userId',verifyToken, deleteUser);
 router.post('/signout',signout)
+router.get('/getusers',verifyToken,getUsers);
 export default router;
