4decc264964170de23c9d38ab3bb4a4a7217510a
Pankaj Khandelwal <pankaj139@gmail.com>
2024-03-02 00:59:43 +0530

    Disable eslint for prop-types in Comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,9 +1,9 @@
+/* eslint-disable react/prop-types */
 import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
-import { set } from 'mongoose';
 
 export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
